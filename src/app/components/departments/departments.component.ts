5b2b2d3b356b99f5acf92963afbfd3a904829af5
Kevin Child <kchild89@gmail.com>
2025-04-07 10:39:59 -0600

    finished part 2 of unit 5

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartmentsService } from '../../services/departments.service';
+import { Department } from '../../interfaces/department';
 
 @Component({
   selector: 'app-departments',
@@ -6,6 +8,14 @@ import { Component } from '@angular/core';
   templateUrl: './departments.component.html',
   styleUrl: './departments.component.scss'
 })
-export class DepartmentsComponent {
+export class DepartmentsComponent implements OnInit {
+  departments: Department[] | undefined;
 
+  constructor(
+    private departmentsService: DepartmentsService
+  ) { }
+  
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+}
 }
