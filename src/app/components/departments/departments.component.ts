b44bf0267633ee8a79d1b69053899a2b797299b4
Kevin Child <kchild89@gmail.com>
2025-04-08 07:32:36 -0600

    Injected router into constructor

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from '../../interfaces/department';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-departments',
@@ -9,10 +10,11 @@ import { Department } from '../../interfaces/department';
   styleUrl: './departments.component.scss'
 })
 export class DepartmentsComponent implements OnInit {
-  departments: Department[] | undefined;
+  departments: Department[];
 
   constructor(
-    private departmentsService: DepartmentsService
+    private departmentsService: DepartmentsService,
+    private router: Router
   ) { }
   
   ngOnInit(): void {
